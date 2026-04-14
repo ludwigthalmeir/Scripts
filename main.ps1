db5c787059669623ddb5f9e759f5e7d3e651decc
ludwigthalmeir <ludwig.thalmeir@gmail.com>
2026-04-14 23:01:47 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -6,11 +6,10 @@ $scriptPath = "/home/ludwig/Dokumente/Scripts"
 # MENU (EINFACH ERWEITERBAR)
 # =========================
 $menu = @(
-    @{ Index = 1; Name = "Repo Update                                   |";        File = "RepoUpdate.ps1" }
-    @{ Index = 2; Name = "Postfach Attribute                            |"; File = "Postfachattribute.ps1" }
-    @{ Index = 3; Name = "Postfach Attribute erweitert                  |"; File = "Postfachattribute erweitert.ps1" }
-    @{ Index = 4; Name = "Alle Postfächer Größe                         |"; File = "Alle Postfächer Größe.ps1" }
-    @{ Index = 4; Name = ""                                             |""; File = "Alle Postfächer Größe.ps1" }
+    @{ Index = 1; Name = "Repo Update";        File = "RepoUpdate.ps1" }
+    @{ Index = 2; Name = "Postfach Attribute"; File = "Postfachattribute.ps1" }
+    @{ Index = 3; Name = "Postfach Attribute erweitert"; File = "Postfachattribute erweitert.ps1" }
+    @{ Index = 4; Name = "Alle Postfächer Größe"; File = "Alle Postfächer Größe.ps1" }
 )
 
 # =========================
@@ -51,7 +50,7 @@ while ($true) {
     Write-Host "==============================================" -ForegroundColor Cyan
     Write-Host "                 ADMIN MAIN                   " -ForegroundColor Cyan
     Write-Host "==============================================" -ForegroundColor Cyan
-    Write-Host "                                             |"
+    Write-Host ""
 
     foreach ($item in $menu) {
         Write-Host ("[{0}] {1}" -f $item.Index, $item.Name)
